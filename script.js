7b00bf20a446a3509848123a14cfc3a789946140
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-11 02:33:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let ans=0;
+	while(arr.length>=2){
+		arr.sort((a,b)=>{
+			if(a<b){
+				return -1;
+			}
+			else if(a>b){
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		})
+		ans+=arr[0]+arr[1];
+		arr.push(arr[0]+arr[1]);
+		arr.splice(0,2);
+	}
+	  return ans;
 }
 
 module.exports=mincost;
